53c984121a39e264d60e8bf363fd160e79ce23fc
Cap-y <carl_mard@hotmail.com>
2016-03-20 22:41:47 +0100

    Lots of shit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222145620) do
+ActiveRecord::Schema.define(version: 20160318004108) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -29,5 +29,15 @@ ActiveRecord::Schema.define(version: 20160222145620) do
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
+
+  create_table "games", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.string   "file_file_name"
+    t.string   "file_content_type"
+    t.integer  "file_file_size"
+    t.datetime "file_updated_at"
+  end
 
 end
